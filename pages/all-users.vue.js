72c1e1a2be98ede9936bd976910e0f052532b719
viktorsbcs <electricjackiebeats@gmail.com>
2022-05-17 07:13:06 +0300

    code cleanup

--- pages/all-users.vue.js
+++ pages/all-users.vue.js
@@ -1,44 +1,35 @@
 var AllUsers = {
   template: `
-        <div class="row mt-5">  
+    <div class="row mt-5">
 
-            <div class="col-3"></div>
-            <div class="col-6">
-            <div class="row pull-right">
-                <button
-                type="button"
-                class="btn btn-primary d-grid gap-2 col-6 mx-auto"
-                @click="restCall"
-                >
+    <div class="col-3"></div>
+    <div class="col-6">
+        <div class="row pull-right">
+            <button type="button" class="btn btn-primary d-grid gap-2 col-6 mx-auto" @click="restCall">
                 Make Rest Call Data
-                </button>
-            </div>
-            </div>
-            <div class="col-3"></div>
+            </button>
         </div>
-
-        <div class="row mt-5">  <!--User list-->
-  <div class="col-12">
-    <div v-for="page_response in response_list">
-      <div class="card-group">
-        <div class="card" v-for="user in page_response.data">
-          <img
-          v-bind:src="user.avatar"
-            class="card-img-top"
-            alt="{{user.first_name}}"
-            style="width: 100%; height: 20vh; object-fit: cover"
-          />
-          <div class="card-body">
-            <h5 class="card-title">{{user.first_name}} {{user.last_name}}</h5>
-            <p class="card-text" style="font-size: smaller">{{user.email}}</p>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-</div>
+    <div class="col-3"></div>
+    </div>
 
-
+    <div class="row mt-5">
+    <!--User list-->
+    <div class="col-12">
+        <div v-for="page_response in response_list">
+            <div class="card-group">
+                <div class="card" v-for="user in page_response.data">
+                    <img v-bind:src="user.avatar" class="card-img-top" alt="{{user.first_name}}"
+                        style="width: 100%; height: 20vh; object-fit: cover" />
+                    <div class="card-body">
+                        <h5 class="card-title">{{user.first_name}} {{user.last_name}}</h5>
+                        <p class="card-text" style="font-size: smaller">{{user.email}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
     `,
   data: function () {
     return {
@@ -58,20 +49,11 @@ var AllUsers = {
   },
   methods: {
     restCall: async function () {
-      console.log("restCall() triggered");
-
       this.response_list = [];
 
       axios
         .get("https://reqres.in/api/user")
-        .then(
-          (response) => (
-            (this.total_pages = response.data.total_pages),
-            console.log(response)
-          )
-        );
-
-      console.log("total pages: " + this.total_pages);
+        .then((response) => (this.total_pages = response.data.total_pages));
 
       var i = 0;
       do {
--- pages/user.vue.js
+++ pages/user.vue.js
@@ -1,259 +1,151 @@
 var User = {
   template: `
-    <div class="row mt-5">  
-        <div class="col-3"></div>
-        <div class="col-6">
-                           
-            <form @submit.prevent="checkForm"   name="checkForm">
+    <div class="row mt-5">
+    <div class="col-3"></div>
+    <div class="col-6">
+
+        <form @submit.prevent="checkForm" name="checkForm">
             <div class="mb-3">
+                <p v-if="errors.length">
+                    <b class="text-danger form-label">Please correct the following error(s):</b>
+                <ul>
+                    <li class="text-danger form-label" v-for="error in errors">{{ error }}</li>
+                </ul>
+                </p>
 
-              <p v-if="errors.length">
-              <b class="text-danger form-label">Please correct the following error(s):</b>
-              <ul>
-                <li class="text-danger form-label" v-for="error in errors">{{ error }}</li>
-              </ul>
-              </p>
-
-
-              <label for="name" class="form-label">Name</label>
-      
-              <input
-                name="name"
-                v-model="user.name"
-                type="text"
-                class="form-control"
-                id="name"
-              />
+                <label for="name" class="form-label">Name</label>
+                <input name="name" v-model="user.name" type="text" class="form-control" id="name" />
 
             </div>
             <div class="mb-3">
-              <label for="email" class="form-label">Email</label>
-      
-              <input
-                name="email"
-                v-model="user.email"
-                type="text"
-                class="form-control"
-                id="email"
-              />
-      
+                <label for="email" class="form-label">Email</label>
+                <input name="email" v-model="user.email" type="text" class="form-control" id="email" />
 
             </div>
             <div class="mb-3">
-              <label for="role" class="form-label">Select user role</label>
-      
-              <select
-                class="form-select"
-                aria-label="user-role"
-                id="role"
-                v-model="user.role"
-                name="role"
-              >
-                <option value="Subscriber">Subscriber</option>
-                <option value="Moderator">Moderator</option>
-                <option value="Administrator">Administrator</option>
-              </select>
+                <label for="role" class="form-label">Select user role</label>
+
+                <select class="form-select" aria-label="user-role" id="role" v-model="user.role" name="role">
+                    <option value="Subscriber">Subscriber</option>
+                    <option value="Moderator">Moderator</option>
+                    <option value="Administrator">Administrator</option>
+                </select>
 
             </div>
-      
-            <button
-              type="submit"
-              class="btn btn-primary"
-            >
-              Submit
+            <button type="submit" class="btn btn-primary">
+                Submit
             </button>
-          </form>
-
-
-
-
-        </div>
-        <div class="col-3"></div>
-
-
-        <div class="row mt-5">
-  
-
-          <div class="col-3"></div>
-          <div class="col-6">
-          
-
-
-
-
-
-          <nav>
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button
-              class="nav-link active"
-              id="nav-all-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#nav-all"
-              type="button"
-              role="tab"
-              aria-controls="nav-all"
-              aria-selected="true"
-            >
-              All
-            </button>
-    
-            <button
-              class="nav-link"
-              id="nav-administrator-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#nav-administrator"
-              type="button"
-              role="tab"
-              aria-controls="nav-administrator"
-              aria-selected="false"
-            >
-              Administrators
-            </button>
-    
-            <button
-              class="nav-link"
-              id="nav-moderator-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#nav-moderator"
-              type="button"
-              role="tab"
-              aria-controls="nav-moderator"
-              aria-selected="false"
-            >
-              Moderators
-            </button>
-    
-            <button
-              class="nav-link"
-              id="nav-subscriber-tab"
-              data-bs-toggle="tab"
-              data-bs-target="#nav-subscriber"
-              type="button"
-              role="tab"
-              aria-controls="nav-subscriber"
-              aria-selected="false"
-            >
-              Subscribers
-            </button>
-          </div>
-        </nav>
-
-              <div class="tab-content" id="nav-tabContent">
-              <div
-                class="tab-pane fade show active"
-                id="nav-all"
-                role="tabpanel"
-                aria-labelledby="nav-all-tab"
-              >
-                <table class="table table-bordered">
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                  </tr>
-                  <tr v-for="user in filterNav(1)">
-                    <td>{{user.userId}}</td>
-                    <td>{{user.name}}</td>
-                    <td>{{user.email}}</td>
-                    <td>{{user.role}}</td>
-                  </tr>
-                </table>
-              </div>
-        
-              <div
-                class="tab-pane fade"
-                id="nav-administrator"
-                role="tabpanel"
-                aria-labelledby="nav-administrator-tab"
-              >
-                <table class="table table-bordered">
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                  </tr>
-                  <tr v-for="user in filterNav(2)">
-                    <td>{{user.userId}}</td>
-                    <td>{{user.name}}</td>
-                    <td>{{user.email}}</td>
-                    <td>{{user.role}}</td>
-                  </tr>
-                </table>
-              </div>
-        
-              <div
-                class="tab-pane fade"
-                id="nav-moderator"
-                role="tabpanel"
-                aria-labelledby="nav-moderator-tab"
-              >
-                <table class="table table-bordered">
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                  </tr>
-                  <tr v-for="user in filterNav(3)">
-                    <td>{{user.userId}}</td>
-                    <td>{{user.name}}</td>
-                    <td>{{user.email}}</td>
-                    <td>{{user.role}}</td>
-                  </tr>
-                </table>
-              </div>
-        
-              <div
-                class="tab-pane fade"
-                id="nav-subscriber"
-                role="tabpanel"
-                aria-labelledby="nav-subscriber-tab"
-              >
-                <table class="table table-bordered">
-                  <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                  </tr>
-                  <tr v-for="user in filterNav(4)">
-                    <td>{{user.userId}}</td>
-                    <td>{{user.name}}</td>
-                    <td>{{user.email}}</td>
-                    <td>{{user.role}}</td>
-                  </tr>
-                </table>
-              </div>
-            </div>
-         
-
-
-
-          
-
-
-
-          
-          </div>
-          <div class="col-3">
-
-            <button
-            type="button"
-            class="btn btn-primary"
-            @click="populateWithDummyData"
-            >Fill with dummy data</button>
-
-          </div>
-
-        </div>
-
-
-
+        </form>
 
 
     </div>
+    <div class="col-3"></div>
+
+    <div class="row mt-5">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <button class="nav-link active" id="nav-all-tab" data-bs-toggle="tab" data-bs-target="#nav-all"
+                        type="button" role="tab" aria-controls="nav-all" aria-selected="true">
+                        All
+                    </button>
+
+                    <button class="nav-link" id="nav-administrator-tab" data-bs-toggle="tab"
+                        data-bs-target="#nav-administrator" type="button" role="tab" aria-controls="nav-administrator"
+                        aria-selected="false">
+                        Administrators
+                    </button>
+
+                    <button class="nav-link" id="nav-moderator-tab" data-bs-toggle="tab" data-bs-target="#nav-moderator"
+                        type="button" role="tab" aria-controls="nav-moderator" aria-selected="false">
+                        Moderators
+                    </button>
+
+                    <button class="nav-link" id="nav-subscriber-tab" data-bs-toggle="tab"
+                        data-bs-target="#nav-subscriber" type="button" role="tab" aria-controls="nav-subscriber"
+                        aria-selected="false">
+                        Subscribers
+                    </button>
+                </div>
+            </nav>
+
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-all" role="tabpanel" aria-labelledby="nav-all-tab">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                        <tr v-for="user in filterNav(1)">
+                            <td>{{user.userId}}</td>
+                            <td>{{user.name}}</td>
+                            <td>{{user.email}}</td>
+                            <td>{{user.role}}</td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="tab-pane fade" id="nav-administrator" role="tabpanel"
+                    aria-labelledby="nav-administrator-tab">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                        <tr v-for="user in filterNav(2)">
+                            <td>{{user.userId}}</td>
+                            <td>{{user.name}}</td>
+                            <td>{{user.email}}</td>
+                            <td>{{user.role}}</td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="tab-pane fade" id="nav-moderator" role="tabpanel" aria-labelledby="nav-moderator-tab">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                        <tr v-for="user in filterNav(3)">
+                            <td>{{user.userId}}</td>
+                            <td>{{user.name}}</td>
+                            <td>{{user.email}}</td>
+                            <td>{{user.role}}</td>
+                        </tr>
+                    </table>
+                </div>
+
+                <div class="tab-pane fade" id="nav-subscriber" role="tabpanel" aria-labelledby="nav-subscriber-tab">
+                    <table class="table table-bordered">
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                        </tr>
+                        <tr v-for="user in filterNav(4)">
+                            <td>{{user.userId}}</td>
+                            <td>{{user.name}}</td>
+                            <td>{{user.email}}</td>
+                            <td>{{user.role}}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <button type="button" class="btn btn-primary" @click="populateWithDummyData">Fill with dummy data</button>
+        </div>
+    </div>
+  </div>
     `,
 
   data: function () {
@@ -288,22 +180,16 @@ var User = {
       }
 
       if (this.errors.length > 0) {
-        console.log("Validation error");
         return true;
       }
 
       if (!this.errors == 0) {
-        console.log(this.user);
-        console.log("Validation success");
         this.userCounter += 1;
         this.user.userId = this.userCounter;
         this.users.push(this.user);
 
         this.user = {};
       }
-
-      console.log("Function end reached");
-      // e.preventDefault();
     },
 
     validEmail: function (email) {
@@ -320,13 +206,6 @@ var User = {
         return this.users.filter((user) => user.role === "Moderator");
       if (setTab === 4)
         return this.users.filter((user) => user.role === "Subscriber");
-
-      console.log(
-        "filterNav() triggered, setTab=" +
-          setTab +
-          ", fillText=" +
-          this.filtText
-      );
     },
 
     populateWithDummyData: function () {
