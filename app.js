f51bc26fa85b9ee788ff5b9ec365a6161666a004
viktorsbcs <electricjackiebeats@gmail.com>
2022-05-17 05:19:18 +0300

    app.js - removing comments

--- app.js
+++ app.js
@@ -3,20 +3,11 @@ const routes = [
   { path: "/user", component: User },
   { path: "/all-users", component: AllUsers },
 ];
-// const router = new VueRouter({
-//   routes: routes,
-//   mode: "history",
-//   base: "/",
-// });
 
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes,
 });
-
-// var app = new Vue({
-//   router: router,
-// }).$mount('#app');
 
 const app = Vue.createApp({});
 app.use(router);
