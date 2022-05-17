c8f72d56ecb68f96fe7ef795ab5b2896ce62066e
viktorsbcs <electricjackiebeats@gmail.com>
2022-05-17 07:06:48 +0300

    all-users.vue - axios looped call

--- pages/all-users.vue.js
+++ pages/all-users.vue.js
@@ -1,5 +1,5 @@
 var AllUsers = {
-    template: `
+  template: `
         <div class="row mt-5">  
 
             <div class="col-3"></div>
@@ -16,52 +16,71 @@ var AllUsers = {
             </div>
             <div class="col-3"></div>
         </div>
-        
-        {{info}}
-        
+
         <div class="row mt-5">  <!--User list-->
-            <div class="col-12">
-            <div ng-repeat="page_response in response_list">
-                <div class="card-group">
-                <div class="card" ng-repeat="user in page_response.data">
-                    <img
-                    src="{{user.avatar}}"
-                    class="card-img-top"
-                    alt="{{user.first_name}}"
-                    style="width: 100%; height: 20vh; object-fit: cover"
-                    />
-                    <div class="card-body">
-                    <h5 class="card-title">{{user.first_name}} {{user.last_name}}</h5>
-                    <p class="card-text" style="font-size: smaller">{{user.email}}</p>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div>
+  <div class="col-12">
+    <div v-for="page_response in response_list">
+      <div class="card-group">
+        <div class="card" v-for="user in page_response.data">
+          <img
+          v-bind:src="user.avatar"
+            class="card-img-top"
+            alt="{{user.first_name}}"
+            style="width: 100%; height: 20vh; object-fit: cover"
+          />
+          <div class="card-body">
+            <h5 class="card-title">{{user.first_name}} {{user.last_name}}</h5>
+            <p class="card-text" style="font-size: smaller">{{user.email}}</p>
+          </div>
         </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     `,
-    data: function () {
-        return {
-          errors: [],
-          user: {
-            userId: null,
-            name: null,
-            email: null,
-            role: null,
-          },
-          userCounter: 0,
-          users: [],
-          info: null
-        };
+  data: function () {
+    return {
+      errors: [],
+      user: {
+        userId: null,
+        name: null,
+        email: null,
+        role: null,
+      },
+      userCounter: 0,
+      users: [],
+      info: null,
+      total_pages: 0,
+      response_list: [],
+    };
+  },
+  methods: {
+    restCall: async function () {
+      console.log("restCall() triggered");
+
+      this.response_list = [];
+
+      axios
+        .get("https://reqres.in/api/user")
+        .then(
+          (response) => (
+            (this.total_pages = response.data.total_pages),
+            console.log(response)
+          )
+        );
+
+      console.log("total pages: " + this.total_pages);
+
+      var i = 0;
+      do {
+        i += 1;
+        axios
+          .get("https://reqres.in/api/users?page=" + i)
+          .then((response) => this.response_list.push(response.data));
+        await new Promise((r) => setTimeout(r, 200));
+      } while (i < this.total_pages);
     },
-    methods: {
-        restCall: function () {
-            console.log("restCall() triggered");
-
-            axios
-            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-            .then(response => (this.info = response))
-
-        },
-    }
-}
+  },
+};
